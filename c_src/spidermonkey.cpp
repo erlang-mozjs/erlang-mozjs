d60d5d800fd78ecab2af25bf40dec442fee40db2
Peter Lemenkov <lemenkov@gmail.com>
2018-07-30 11:36:03 +0200

    Remove unnecessary include
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- c_src/spidermonkey.cpp
+++ c_src/spidermonkey.cpp
@@ -13,7 +13,6 @@
    See the License for the specific language governing permissions and
    limitations under the License. */
 
-#include <stdio.h>
 #include <unistd.h>
 #include <time.h>
 #include <erl_driver.h>
