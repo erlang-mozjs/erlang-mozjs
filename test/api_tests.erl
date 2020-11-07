e452e9a66508270d2e53b4bdf844050be26f10e9
Peter Lemenkov <lemenkov@gmail.com>
2020-11-07 23:20:02 +0100

    Redo interrupt tests
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- test/api_tests.erl
+++ test/api_tests.erl
@@ -3,33 +3,38 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-endless_loop_basic_interrupt_test() ->
-	{ok, Handle} = mozjs_nif:sm_init(8, 8),
-	js:define(Handle, <<"function infinite_foo_1() { for (;;) {} };">>),
+interrupt_test_() ->
+	{setup,
+		fun() -> {ok, Handle} = mozjs_nif:sm_init(8, 8), js:define(Handle, <<"function infinite_foo() { for (;;) {} };">>), Handle end,
+		fun(Handle) -> mozjs_nif:sm_stop(Handle) end,
+		fun(Handle) -> [
+		        {"Basic interrupt", ?_assertEqual(ok, basic_interrupt(Handle))},
+		        {"Interrupt with return value", ?_assertEqual({error, mozjs_script_interrupted}, interrupt_with_return_value(Handle))}
+		] end
+      }.
+
+basic_interrupt(Handle) ->
 	spawn(
 		fun() ->
-			catch js:eval(Handle, <<"infinite_foo_1();">>)
+			catch js:eval(Handle, <<"infinite_foo();">>)
 		end
 	),
 	timer:sleep(500),
-	?assertEqual(ok, mozjs_nif:sm_cancel(Handle)).
+	mozjs_nif:sm_cancel(Handle).
 
-endless_loop_basic_interrupt_with_return_test() ->
-	{ok, Handle} = mozjs_nif:sm_init(8, 8),
-	js:define(Handle, <<"function infinite_foo_2() { for (;;) {} };">>),
+interrupt_with_return_value(Handle) ->
 	Pid = self(),
 	spawn(
 		fun() ->
-			Ret = (catch js:eval(Handle, <<"infinite_foo_2();">>)),
+			Ret = (catch js:eval(Handle, <<"infinite_foo();">>)),
 			ok = mozjs_nif:sm_cancel(Handle),
 			Pid ! Ret
 		end
 	),
 	timer:sleep(500),
 	mozjs_nif:sm_cancel(Handle),
-	Ret = receive
+	receive
 		Something -> Something
 	after
 		1000 -> timeout
-	end,
-	?assertEqual({error, mozjs_script_interrupted}, Ret).
+	end.
